a61e4a27d891cd906729731b35ea7071bd2a9b47
Культяпов Константин <pozys@mail.ru>
2024-03-11 22:13:14 +0300

    add TaskRequest

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:1', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'status_id' => ['required', 'integer', Rule::exists('task_statuses', 'id')],
+            'created_by_id' => ['required', 'integer', Rule::exists('users', 'id')],
+            'assigned_to_id' => ['nullable', 'integer', Rule::exists('users', 'id')],
+        ];
+    }
+}
