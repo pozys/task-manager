7ef746951ac078c74370c5ac46438094d6f6229e
Культяпов Константин <pozys@mail.ru>
2024-03-11 21:54:22 +0300

    add TaskPolicy

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class TaskPolicy
+{
+    public function viewAny(?User $user): bool
+    {
+        return true;
+    }
+
+    public function view(): bool
+    {
+        return Auth::check();
+    }
+
+    public function create(): bool
+    {
+        return Auth::check();
+    }
+
+    public function update(): bool
+    {
+        return Auth::check();
+    }
+
+    public function delete(User $user, Task $task): bool
+    {
+        return $task->created_by_id === $user->id;
+    }
+
+    public function forceDelete(): bool
+    {
+        return false;
+    }
+}
