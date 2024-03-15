1fc646229cc453092dd8a36250b9a06f2edd9e0f
Культяпов Константин <pozys@mail.ru>
2024-03-15 23:21:28 +0300

    refactor TaskStatus views

--- resources/views/components/task-status/form.blade.php
+++ resources/views/components/task-status/form.blade.php
@@ -1,24 +1,20 @@
-<section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-        <div class="grid col-span-full">
-            <h1 class="mb-5">{{ $title }}</h1>
+<x-forms.entity>
+    <h1 class="mb-5">{{ $title }}</h1>
 
-            {{ html()->modelForm($taskStatus ?? null, $method, $action)->open() }}
-            <div class="flex flex-col">
-                <div>
-                    {{ html()->label( __('task-status.name'), 'name') }}
-                </div>
-                <div class="mt-2">
-                    {{ html()->text('name')->class('rounded border-gray-300 w-1/3') }}
-                    @error('name')
-                    <x-forms.validation :$message />
-                    @enderror
-                </div>
-                <div class="mt-2">
-                    {{ $submit }}
-                </div>
-            </div>
-            {{ html()->closeModelForm() }}
+    {{ html()->modelForm($taskStatus ?? null, $method, $action)->open() }}
+    <div class="flex flex-col">
+        <div>
+            {{ html()->label( __('task-status.name'), 'name') }}
+        </div>
+        <div class="mt-2">
+            {{ html()->text('name')->class('rounded border-gray-300 w-1/3') }}
+            @error('name')
+            <x-forms.validation :$message />
+            @enderror
+        </div>
+        <div class="mt-2">
+            {{ $submit }}
         </div>
     </div>
-</section>
+    {{ html()->closeModelForm() }}
+</x-forms.entity>
--- resources/views/task_statuses/edit.blade.php
+++ resources/views/task_statuses/edit.blade.php
@@ -1,8 +1,6 @@
-<x-app-layout>
-    <x-task-status.form method='PUT' :action="route('task_statuses.update', $taskStatus)"
-        :title="__('task-status.edit.title')" :$taskStatus>
-        <x-slot:submit>
-            <x-forms.submit :text="__('task-status.edit.submit')" />
-            </x-slot>
-    </x-task-status.form>
-</x-app-layout>
+<x-task-status.form method='PUT' :action="route('task_statuses.update', $taskStatus)"
+    :title="__('task-status.edit.title')" :$taskStatus>
+    <x-slot:submit>
+        <x-forms.submit :text="__('task-status.edit.submit')" />
+        </x-slot>
+</x-task-status.form>
