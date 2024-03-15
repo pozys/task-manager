eb4fbc6d8ee91ac809d39008d7e1cb212b829420
Культяпов Константин <pozys@mail.ru>
2024-03-15 22:05:40 +0300

    update TaskStatusFactory

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -9,7 +9,7 @@ class TaskStatusFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->text(30),
         ];
     }
 }
