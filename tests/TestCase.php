07edc8bfe8c1d71a24b110f22fde52cf5eb73c0e
Культяпов Константин <pozys@mail.ru>
2024-03-15 22:07:48 +0300

    replace RefreshDatabase to DatabaseTransactions in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,13 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use RefreshDatabase;
+    use DatabaseTransactions;
     use WithFaker;
 }
