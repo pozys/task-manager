595b523254c66654b3c29f13b402d78ae60fcccd
Культяпов Константин <pozys@mail.ru>
2024-03-15 22:07:14 +0300

    create TaskTest

---
+++ tests/Feature/Http/Controllers/TaskTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Task;
+use Tests\ControllerTestCase;
+
+class TaskTest extends ControllerTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actingAs($this->user);
+    }
+    public function testIndex(): void
+    {
+        $response = $this->get(route('tasks.index'));
+        $response->assertOk();
+        $response->assertSee(__('task.index.id'));
+        $response->assertSee(__('task.index.status'));
+        $response->assertSee(__('task.index.name'));
+        $response->assertSee(__('task.index.created_by'));
+        $response->assertSee(__('task.index.assigned_to'));
+        $response->assertSee(__('task.index.created_at'));
+        $response->assertSee(__('task.index.actions'));
+    }
+
+    public function testCreate(): void
+    {
+        $task = Task::factory()->make();
+        $taskData = $task->only($task->getFillable());
+        $response = $this->post(route('tasks.store'), $taskData);
+
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('tasks', $taskData);
+    }
+
+    public function testUpdate(): void
+    {
+        $task = $this->createTask();
+        $taskParams = $task->only($task->getFillable());
+        $taskParams['name'] = $this->faker->name;
+
+        $response = $this->put(route('tasks.update', compact('task')), $taskParams);
+
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('tasks', array_merge(['id' => $task->id], $taskParams));
+    }
+
+    public function testDestroy(): void
+    {
+        $task = $this->createTask();
+
+        $response = $this->delete(
+            route('tasks.destroy', compact('task'))
+        );
+
+        $response->assertSessionDoesntHaveErrors();
+        $response->assertRedirect();
+
+        $this->assertSoftDeleted($task);
+    }
+
+    private function createTask(): Task
+    {
+        return Task::factory()->create();
+    }
+}
